b40f428090cee9d6da08dc6f19b3a011e7a2fe32
M00nlightbee <149159857+M00nlightbee@users.noreply.github.com>
2025-01-02 15:14:29 +0000

    Add OrderDetails class to Join Product and Orders table

---
+++ AIMS/Models/OrderDetails.cs
@@ -0,0 +1,17 @@
+﻿using AIMS.Data;
+
+namespace AIMS.Models
+{
+    public class OrderDetails
+    {
+        //public Product product { get; set; }
+        //public Orders orders { get; set; }
+
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductSize { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
