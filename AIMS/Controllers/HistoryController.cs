4f46c93c155df9a7c03006b02bd02ac7e6312ec2
M00nlightbee <149159857+M00nlightbee@users.noreply.github.com>
2025-01-03 21:06:41 +0000

    Added HistoryController to manage order history display

---
+++ AIMS/Controllers/HistoryController.cs
@@ -0,0 +1,23 @@
+﻿using AIMS.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AIMS.Controllers
+{
+	public class HistoryController : Controller
+	{
+		//Data access for order history
+		private readonly HistoryData _dataAccess;
+		public HistoryController(HistoryData dataAccess)
+		{
+			_dataAccess = dataAccess;
+		}
+
+		//Display all order history
+		public IActionResult Index()
+		{
+			var orderHistory = _dataAccess.GetOrderHistory();
+			return View(orderHistory);
+		}
+
+	}
+}
