101d6fa7348e29db96fd3ae7df495730ddb76f17
M00nlightbee <149159857+M00nlightbee@users.noreply.github.com>
2025-01-04 21:14:06 +0000

    Added Analytics class to model folder

---
+++ AIMS/Models/Analytics.cs
@@ -0,0 +1,9 @@
+﻿namespace AIMS.Models
+{
+	public class Analytics
+	{
+		public int TotalQuantity { get; set; }
+		public Dictionary<string, int> StockByBranch { get; set; } = new Dictionary<string, int>();
+		public Dictionary<string, int> StockByCategory { get; set; } = new Dictionary<string, int>();
+	}
+}
